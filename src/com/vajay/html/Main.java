ebb6c22ca6f310e7980a2723686ac5e9f260c07d
László Miklós Vajay <vajaylm@gmail.com>
2023-04-02 10:51:13 +0200

    Add main method to test the API

---
+++ src/com/vajay/html/Main.java
@@ -0,0 +1,69 @@
+package com.vajay.html;
+
+import com.vajay.html.api.HtmlDocument;
+import com.vajay.html.api.HtmlHeader;
+import com.vajay.html.api.HtmlLink;
+import com.vajay.html.api.HtmlParagraph;
+import com.vajay.html.api.HtmlTable;
+import com.vajay.html.api.HtmlTableCell;
+import com.vajay.html.api.HtmlTableRow;
+import com.vajay.html.api.HtmlTitle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("A név vagy az email paraméter nem lett megadva!");
+			return;
+		}
+		
+		String nameParam = args[0];
+		String emailParam = args[1];
+		String repositoryUrl = "https://github.com/Vajaylm/HtmlGenerator";
+		
+		int indentSpaceCount = 5;
+		
+		HtmlDocument doc = new HtmlDocument(indentSpaceCount);
+		doc.addHeadElement(new HtmlTitle(nameParam + " - Teszt Feladat"));
+		
+		HtmlHeader header1 = new HtmlHeader("Teszt Feladat", 1);
+		doc.addBodyElement(header1);
+		
+		HtmlHeader header2 = new HtmlHeader("Nem látszó header (h2)", 2);
+		doc.addBodyElement(header2);
+		
+		HtmlParagraph paragraph1 = new HtmlParagraph();
+		doc.addBodyElement(paragraph1);
+		HtmlLink link1 = new HtmlLink("Megoldás");
+		link1.addProperty("href", repositoryUrl);
+		paragraph1.addChildElement(link1);
+		
+		doc.removeBodyElement(header2);
+		
+		HtmlParagraph paragraph2 = new HtmlParagraph("A feladat elkészítőjének adatai");
+		doc.addBodyElement(paragraph2);
+
+		HtmlTable table = new HtmlTable();
+		table.addProperty("border", "1px solid black");
+		HtmlTableRow row1 = new HtmlTableRow();
+		table.addChildElement(row1);
+		row1.addChildElement(new HtmlTableCell("Név"));
+		row1.addChildElement(new HtmlTableCell(nameParam));
+		HtmlTableCell cell1 = new HtmlTableCell("Törlendő cella");
+		row1.addChildElement(cell1);
+
+		HtmlTableRow row2 = new HtmlTableRow();
+		table.addChildElement(row2);
+		row2.addChildElement(new HtmlTableCell("Elérhetőség"));
+		row2.addChildElement(new HtmlTableCell(emailParam));
+		doc.addBodyElement(table);
+
+		HtmlLink link2 = new HtmlLink("L&P Solutions");
+		link2.addProperty("href", "http://lpsolutions.hu");
+		doc.addBodyElement(link2);
+		
+		row1.removeChildElement(cell1);
+		System.out.println(doc.toString());
+	}
+
+}
