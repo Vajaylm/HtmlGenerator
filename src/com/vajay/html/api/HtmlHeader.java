d0868754734b418d1d56d3edf511d99f6014aa4a
László Miklós Vajay <vajaylm@gmail.com>
2023-04-02 18:04:05 +0200

    Throw exception if Header level is out of 1-6 range

--- src/com/vajay/html/api/HtmlHeader.java
+++ src/com/vajay/html/api/HtmlHeader.java
@@ -1,10 +1,15 @@
 package com.vajay.html.api;
 
+import java.security.InvalidParameterException;
+
 public class HtmlHeader extends HtmlElement {
 	private int level;
 
 	public HtmlHeader(String text, int level) {
 		super(text);
+		if (level < 1 || level > 6) {
+			throw new InvalidParameterException("A Header szintje 1 - 6 közötti szám lehet csak!");
+		}
 		this.level = level;
 	}
 
