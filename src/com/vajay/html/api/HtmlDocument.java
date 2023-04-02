a28fa4b4623ea0ee3260a478314f1e7deff11bbc
László Miklós Vajay <vajaylm@gmail.com>
2023-04-02 09:27:49 +0200

    Add HtmlDocument class

---
+++ src/com/vajay/html/api/HtmlDocument.java
@@ -0,0 +1,50 @@
+package com.vajay.html.api;
+
+public class HtmlDocument {
+	private HtmlHead head;
+	private HtmlBody body;
+	private int indentSpaceCount;
+	private int indentDepth;
+	
+	{
+		this.indentDepth = 0;
+		this.head = new HtmlHead(this.indentSpaceCount, this.indentDepth + 1);
+		this.body = new HtmlBody(this.indentSpaceCount, this.indentDepth + 1);
+	}
+
+	public HtmlDocument(int indentSpaceCount) {
+		this.indentSpaceCount = indentSpaceCount;
+	}
+	
+	public void addHeadElement(HtmlElement element) {
+		if (element == null) {
+			return;
+		}
+		
+		this.head.addChildElement(element);
+	}
+	
+	public void addBodyElement(HtmlElement element) {
+		if (element == null) {
+			return;
+		}
+		
+		this.body.addChildElement(element);
+	}
+	
+	public void removeHeadElement(HtmlElement element) {
+		if (element == null) {
+			return;
+		}
+		
+		this.head.removeChildElement(element);
+	}
+	
+	public void removeBodyElement(HtmlElement element) {
+		if (element == null) {
+			return;
+		}
+		
+		this.body.removeChildElement(element);
+	}
+}
