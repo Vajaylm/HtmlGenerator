72443bb3104c32a1951c32123b0bb9a62bc79e8b
László Miklós Vajay <vajaylm@gmail.com>
2023-04-02 08:52:41 +0200

    Add HtmlElement class with planned constructors

---
+++ src/com/vajay/html/api/HtmlElement.java
@@ -0,0 +1,38 @@
+package com.vajay.html.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class HtmlElement {
+	protected Map<String, String> properties;
+	protected List<HtmlElement> childElements;
+	protected int indentSpaceCount;
+	protected int indentDepth;
+	protected String text;
+
+	{
+		properties = new HashMap<>();
+		childElements = new ArrayList<>();
+		this.indentSpaceCount = 5;
+		this.indentDepth = 1;
+		this.text = null;
+	}
+
+	public HtmlElement(String text) {
+		this.text = text;
+	}
+
+	public HtmlElement(int indentSpaceCount, int indentDepth) {
+		this.indentSpaceCount = indentSpaceCount;
+		this.indentDepth = indentDepth;
+	}
+
+	public HtmlElement(int indentSpaceCount, int indentDepth, String text) {
+		this.indentSpaceCount = indentSpaceCount;
+		this.indentDepth = indentDepth;
+		this.text = text;
+	}
+
+}
